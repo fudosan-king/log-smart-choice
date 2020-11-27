8e2e0901cfbd948510e3c55b08e4a2f4aff0ffbe
Truong Tam <tam@propolife.co.jp>
2020-11-27 16:14:29 +0700

    Update check PHP

--- test_case.sh
+++ test_case.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./vendor/phpcheckstyle/phpcheckstyle/phpcheckstyle --src . --exclude ./vendor --exclude ./database --exclude ./storage --exclude ./resources/views --outdir report --config ./check_php.xml
+./vendor/phpcheckstyle/phpcheckstyle/phpcheckstyle --src . --exclude ./vendor --exclude ./database/migrations --exclude ./storage --exclude ./resources/views --outdir report --config ./check_php.xml
 ./vendor/bin/phpunit
