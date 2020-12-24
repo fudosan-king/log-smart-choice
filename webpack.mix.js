c031ca503fb56bf5eef6a4bcbd430cf1fcdc7fb6
Truong Tam <tam@propolife.co.jp>
2020-12-24 14:20:52 +0700

    Fix webpack.mix.js build fail

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
-
+mix.js('resources/js/app.js', 'public/js');
+mix.styles('resources/css/app.css', 'public/css/app.css');
 mix.styles('resources/css/estates_images.css', 'public/css/estate.css');
