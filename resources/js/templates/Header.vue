98ae4421401de5f3ca335f06dae687e6c3f88fe8
hungnt92-propolile <hungnt92@propolife.co.jp>
2021-01-05 16:26:17 +0700

    Logスマートチョイス　開発　ログインページの仕様

--- resources/js/templates/Header.vue
+++ resources/js/templates/Header.vue
@@ -7,7 +7,16 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">会社概要</a>
                     </li>
-                    <li class="nav-item">
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link">ようこそ {{ customerInfo.name }} 様。</a>
+                    </li>
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link" v-on:click="logout">
+                            <img alt="" class="img-fluid" width="16" /> ログアウト
+                        </a>
+                    </li>
+
+                    <li v-else class="nav-item">
                         <a class="nav-link" v-bind:href="'/login'"><span>ログイン</span></a>
                     </li>
                 </ul>
@@ -16,13 +25,24 @@
     </header>
     <header v-else-if="page === 'detail'">
         <div class="navbar navbar-expand-lg bsnav bsnav-transparent">
-            <a class="navbar-brand" v-bind:href="'/'"><img v-bind:src="logoBlack" alt="" class="img-fluid" width="217"></a>
+            <a class="navbar-brand" v-bind:href="'/'"
+                ><img v-bind:src="logoBlack" alt="" class="img-fluid" width="217"
+            /></a>
             <div class="collapse navbar-collapse justify-content-md-end">
                 <ul class="navbar-nav navbar-mobile mr-0">
                     <li class="nav-item">
                         <a class="nav-link" href="#">会社概要</a>
                     </li>
-                    <li class="nav-item">
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link">ようこそ {{ customerInfo.name }} 様。</a>
+                    </li>
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link" v-on:click="logout">
+                            <img alt="" class="img-fluid" width="16" /> ログアウト
+                        </a>
+                    </li>
+
+                    <li v-else class="nav-item">
                         <a class="nav-link" v-bind:href="'/login'"><span>ログイン</span></a>
                     </li>
                 </ul>
@@ -31,14 +51,25 @@
     </header>
     <header class="sub_header" v-else>
         <div class="navbar navbar-expand-lg bsnav">
-            <a class="navbar-brand" v-bind:href="'/'"><img v-bind:src="logoBlack" alt="" class="img-fluid" width="217"></a>
+            <a class="navbar-brand" v-bind:href="'/'"
+                ><img v-bind:src="logoBlack" alt="" class="img-fluid" width="217"
+            /></a>
             <button class="navbar-toggler toggler-spring"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse justify-content-md-end">
                 <ul class="navbar-nav navbar-mobile mr-0">
                     <li class="nav-item">
                         <a class="nav-link" href="#">会社概要</a>
                     </li>
-                    <li class="nav-item">
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link">ようこそ {{ customerInfo.name }} 様。</a>
+                    </li>
+                    <li v-if="customerInfo.name" class="nav-item nav-header">
+                        <a class="nav-link" v-on:click="logout">
+                            <img alt="" class="img-fluid" width="16" /> ログアウト
+                        </a>
+                    </li>
+
+                    <li v-else class="nav-item">
                         <a class="nav-link" v-bind:href="'/login'"><span>ログイン</span></a>
                     </li>
                 </ul>
