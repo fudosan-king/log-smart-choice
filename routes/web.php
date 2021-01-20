4daeb22eff3a2bea7002bdf547abc3a5f3f1f4c4
hungnt <hungnt92@propolife.co.jp>
2021-01-20 08:33:48 +0700

    LSC　案件の解決策を調べて修正する｜案件の修正依頼（Hungさんの件）

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route to handle page reload in Vue except for api routes
-Route::get('/{any?}', function () {
-   return view('app');
-})->where('any', '^(?!(api|admin))[\/\w\.-]*');
+Route::get('/{any?}', [AppController::class, 'get'])
+    ->where('any', '^(?!(api|admin))[\/\w\.-]*');
 
 
 Route::group(['prefix' => 'admin'], function () {
