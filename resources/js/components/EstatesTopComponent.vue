0b32f01dd8be1c6b677f1931419773fec7ba3178
vannt <vannt97@propolife.co.jp>
2021-03-10 16:31:29 +0700

    #8120 : add estate to wishlist

--- resources/js/components/EstatesTopComponent.vue
+++ resources/js/components/EstatesTopComponent.vue
@@ -4,11 +4,21 @@
             <div class="box_property_item">
                 <div class="box_property_item_img">
                     <a v-bind:href="'/detail/' + estate._id">
-                        <img v-bind:src="estate['photo_first'] ? estate.photo_first : '/images/no-image.png'" alt="" class="img-fluid">
+                        <img v-bind:src="estate['photo_first'] ? estate.photo_first : '/images/no-image.png'" alt=""
+                             class="img-fluid">
                     </a>
                 </div>
                 <div class="box_property_item_body">
-                    <h2><a v-bind:href="'/detail/' + estate._id">{{ estate.custom_field ? estate.custom_field.content : "" }}</a></h2>
+                    <h2>
+                        <a v-bind:href="'/detail/' + estate._id">{{
+                                estate.custom_field ? estate.custom_field.content : ""
+                            }}</a>
+                        <template v-if="customer.is_logged">
+                            <a @click="addToWishList(estate._id, estate.is_wish)">
+                                <WishlistComponent :data-wished="estate.is_wish"></WishlistComponent>
+                            </a>
+                        </template>
+                    </h2>
                     <div class="row">
                         <div class="col-12 col-lg-6">
                             <p>{{ estate.room_count }}{{ estate.service_rooms != '0' ? 'S' : '' }}{{ estate.room_kind }} / {{estate.tatemono_menseki }}㎡</p>
@@ -27,58 +37,97 @@
 </template>
 
 <script>
-	export default {
-        data() {
-            return {
-                estates: [],
-            }
-        },
-        beforeMount() {
-            this.getListEstates();
-        },
-        methods: {
-            getListEstates(){
-                let accessToken = this.$getCookie('accessToken');
-                let auth = {
+import WishlistComponent from '../components/WishlistComponent'
+
+export default {
+    data() {
+        return {
+            estates: [],
+            customer: {},
+        }
+    },
+
+    components: {
+        WishlistComponent
+    },
+    beforeMount() {
+        this.getListEstates();
+    },
+    methods: {
+        getListEstates() {
+            let accessToken = this.$getCookie('accessToken');
+            let auth = {
                     username: `${process.env.MIX_BASIC_AUTH_USERNAME}`,
                     password: `${process.env.MIX_BASIC_AUTH_PASSWORD}`,
                 }
                 if (accessToken != '') {
                     axios({
-                        url: '/customer', 
-                        method: 'POST', 
-                        data: {}, 
+                        url: '/customer',
+                        method: 'POST',
+                        data: {},
                         headers: {
                             'content-type': 'application/json',
                             'AuthorizationBearer': `Bearer ${accessToken}`,
                         },
                         auth: auth,
-                        })
-                        .then(resp => {
-                            let emailCustomer = resp.data.customer.email;
-                            axios({url: '/list', method: 'POST', data: {'limit': 10, 'page': 1, 'email' : emailCustomer}})
-                                .then(resp => {
-                                    this.estates = this.estates.concat(resp.data['data']);
-                                })
-                                .catch(err => {
+                })
+                    .then(resp => {
+                        let emailCustomer = resp.data.customer.email;
+                        this.customer = resp.data.customer
+                        axios({url: '/list', method: 'POST', data: {'limit': 10, 'page': 1, 'email': emailCustomer}})
+                            .then(resp => {
+                                this.estates = this.estates.concat(resp.data['data']);
+                            })
+                            .catch(err => {
                                     console.log('Can not get list estates');
                                 }
                             );
-                        })
-                        .catch(err => {});
-                } else {
-                    axios({url: '/list', method: 'POST', data: {'limit': 10, 'page': 1}})
-                        .then(resp => {
-                            this.estates = this.estates.concat(resp.data['data']);
-                            if (resp.data['data'].length) {
-                            }
-                        })
-                        .catch(err => {
+                    })
+                    .catch(err => {
+                    });
+            } else {
+                axios({url: '/list', method: 'POST', data: {'limit': 10, 'page': 1}})
+                    .then(resp => {
+                        this.estates = this.estates.concat(resp.data['data']);
+                        if (resp.data['data'].length) {
+                        }
+                    })
+                    .catch(err => {
                             console.log('Can not get list estates');
                         }
                     );
+            }
+        },
+        // Add states to wishlist
+        addToWishList(estateId, isWish) {
+            let accessToken = this.$getCookie('accessToken')
+            let auth = {
+                username: `${process.env.MIX_BASIC_AUTH_USERNAME}`,
+                password: `${process.env.MIX_BASIC_AUTH_PASSWORD}`,
+            }
+            if (accessToken != '') {
+                let data = {
+                    estateId: estateId,
+                    is_wish: 1,
                 }
-            },
-        }
-	};
+                if (isWish === 1) {
+                    data = {
+                        estateId: estateId,
+                        is_wish: 0,
+                    }
+                }
+                axios.post("/wishlist", data, {
+                    headers: {
+                        'content-type': 'application/json',
+                        'AuthorizationBearer': `Bearer ${accessToken}`,
+                    },
+                    auth: auth,
+                }).then((res) => {
+                }, (error) => {
+                });
+            }
+        },
+    },
+
+};
 </script>
