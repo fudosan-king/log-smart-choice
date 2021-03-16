7ca452fc9e7ef9f2cb66384e3a1e2fbfcfdd74ec
vannt <vannt97@propolife.co.jp>
2021-03-16 14:58:18 +0700

    fix spam submit form

--- resources/js/pages/ForgotPassword.vue
+++ resources/js/pages/ForgotPassword.vue
@@ -8,7 +8,7 @@
                             <h2>パスワード再設定</h2>
                             <p>下記に登録したメールアドレスを入力してください。パスワード再設定のご案内をメールでお送りします。</p>
                             <p>メールアドレス</p>
-                            <form autocomplete="off" @submit.prevent="forgotPassword()" class="frm_login">
+                            <form autocomplete="off"  class="frm_login">
                                 <div class="form-group">
                                     <div class="row">
                                         <div class="col-12 col-lg-12 col-md-12 align-self-center">
@@ -36,7 +36,7 @@
                                     <div class="row">
                                         <div class="col-12 col-lg-8 offset-lg-2">
                                             <div class="form-group text-center">
-                                                <button type="button" class="btn btnlogin " @click="forgotPassword()">
+                                                <button type="button" class="btn btnlogin " @click="forgotPassword()" :disabled="disabled">
                                                     >メールを送信する
                                                 </button>
                                                 <p class="text-center">
@@ -62,15 +62,19 @@ export default {
         return {
             email: null,
             errors: [],
+            disabled: false,
         };
     },
     methods: {
         forgotPassword() {
+            this.disabled = true ;
             this.errors = [];
             if (!this.email) {
                 this.errors.push('メールアドレスを入力してください');
+                this.disabled = false ;
             } else if (!this.validEmail(this.email)) {
                 this.errors.push('メールが必要です.');
+                this.disabled = false ;
             }
             if (!this.errors.length) {
                 axios.post("/forgot-password", {email: this.email}, {
@@ -78,9 +82,9 @@ export default {
                         'content-type': 'application/json',
                     },
                 }).then(res => {
+                    this.disabled = false ;
                     if (res.data.status == true) {
-                        this.$router.push({name: 'login'}, () => {
-                        })
+                        this.$router.push({name: 'login'});
                     } else{
                         this.errors.push(res.data.message);
                     }
