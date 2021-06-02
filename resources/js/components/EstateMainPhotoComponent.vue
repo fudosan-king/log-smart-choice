e941207bd2c11a3f45a832a3d8ac995291c17e15
hungnt <hungnt92@propolife.co.jp>
2021-06-02 13:46:41 +0700

    #9266: Fix main photo

--- resources/js/components/EstateMainPhotoComponent.vue
+++ resources/js/components/EstateMainPhotoComponent.vue
@@ -1,37 +1,66 @@
 <template>
-<div class="row">
-    <div class="col-sm-3"></div>
-    <div class="col-sm-6">
-        <img class="estate_image_url main-photo" v-bind:src="url_path" />
-        <input class="estate_image_file" name="estate_main_photo" type="file" @change="onFileChange"/>
+    <div class="form-group hp_photo_wrap" id="hp_photo_wrap">
+        <ul id="sortable">
+            <draggable @start="drag = true" @end="drag = false">
+                <li id="imageInfo" v-for="(image, idx) in images" :key="idx">
+                    <h3>Image</h3>
+                    <div class="img-wrap" style="text-align: right;">
+                        <a class="remove-image" @click="removeImage(idx)">&times;</a>
+                    </div>
+                    <img class="estate_image_url image-photo" v-bind:src="image[0]" />
+                    <input
+                        class="estate_image_file"
+                        name="estate_main_photo[]"
+                        type="file"
+                        @change="onFileChange"
+                        v-bind:data-index-image="idx"
+                    />
+                    <input name="estate_main_photo_hidden[]" type="hidden" v-bind:value="image[0]" />
+                </li>
+            </draggable>
+        </ul>
+        <button type="button" class="btn btn-primary append-image" @click="addImage">Append Images</button>
     </div>
-    <div class="col-sm-3"></div>
-</div>
 </template>
 
 <script>
-    export default {
-        props: ['data'],
-        data(){
-            let url_path = '/images/image-main.jpeg';
-            if (Object.keys(JSON.parse(this.data))) {
-                if (typeof JSON.parse(this.data).estate_main_photo != 'undefined') {
-                    url_path = (JSON.parse(this.data)).estate_main_photo;
-                }
-            }
-
-            return {
-                url_path: url_path
-            }
-        },
-        methods: {
-            onFileChange(e) {
-                const file = e.target.files[0];
-                if(file){
-                    this.url_path = URL.createObjectURL(file);
-                    this.$forceUpdate();
+import draggable from 'vuedraggable';
+export default {
+    components: {
+        draggable
+    },
+    props: ['data'],
+    data() {
+        let images = [];
+        let data = JSON.parse(this.data);
+        if (Object.keys(data)) {
+            if (typeof data.estate_main_photo != 'undefined') {
+                const mainPhoto = data.estate_main_photo;
+                for (let i = 0; i < mainPhoto.length; i++) {
+                    const url = mainPhoto[i]['url_path'];
+                    images.push([url, mainPhoto[i]['description']]);
                 }
             }
         }
-    };
+
+        return {
+            images: images
+        };
+    },
+    methods: {
+        addImage() {
+            this.images.push(['/images/no-image.png', '']);
+        },
+        removeImage(idx) {
+            this.$delete(this.images, idx);
+        },
+        onFileChange(e) {
+            const file = e.target.files[0];
+            if (file) {
+                this.images[e.target.dataset.indexImage][0] = URL.createObjectURL(file);
+                this.$forceUpdate();
+            }
+        }
+    }
+};
 </script>
