f1839b4900f0187a8c84d6ff5082d60cdeaf0f82
hungnt <hungnt92@propolife.co.jp>
2021-07-13 19:40:34 +0700

    #9977: Add ckeditor

--- resources/js/components/EstateImagesComponent.vue
+++ resources/js/components/EstateImagesComponent.vue
@@ -1,64 +1,81 @@
 <template>
-<div class="form-group hp_photo_wrap" id="hp_photo_wrap">
-    <ul id="sortable">
-        <draggable @start="drag=true" @end="drag=false">
-        <li class="imageInfo" v-for="(image, idx) in images" :key="idx">
-            <h3>Image</h3>
-            <div class="img-wrap" style="text-align: right;">
-                <a class="remove-image" @click="removeImage(idx)">&times;</a>
-            </div>
-            <img class="estate_image_url image-photo" v-bind:src=image[0] />
-            <input class="estate_image_file" name="estate_image[]" type="file" @change="onFileChange" v-bind:data-index-image=idx />
-            <input name="estate_image_hidden[]" type="hidden" v-bind:value=image[0] />
-            <div class="photo_info">
-                <textarea class="form-control" rows="10" cols="40" name="description[]" >{{ image[1] }}</textarea>
-            </div>
-        </li>
-        </draggable>
-    </ul>
-    <button type="button" class="btn btn-primary append-image" @click="addImage">Append Images</button>
-</div>
+    <div class="form-group hp_photo_wrap" id="hp_photo_wrap">
+        <ul id="sortable">
+            <!-- <draggable @start="drag = true" @end="drag = false"> -->
+            <li class="imageInfo" v-for="(image, idx) in images" :key="idx">
+                <h3>Image</h3>
+                <div class="img-wrap" style="text-align: right;">
+                    <a class="remove-image" @click="removeImage(idx)">&times;</a>
+                </div>
+                <img class="estate_image_url image-photo" v-bind:src="image[0]" />
+                <input
+                    class="estate_image_file"
+                    name="estate_image[]"
+                    type="file"
+                    @change="onFileChange"
+                    v-bind:data-index-image="idx"
+                />
+                <input name="estate_image_hidden[]" type="hidden" v-bind:value="image[0]" />
+                <div class="photo_info">
+                    <textarea class="form-control" rows="10" cols="40" name="description[]" style="display:none;">{{
+                        image[1]
+                    }}</textarea>
+                    <ckeditor v-model="image[1]" :config="editorConfig" tag-name="textarea"></ckeditor>
+                </div>
+            </li>
+            <!-- </draggable> -->
+        </ul>
+        <button type="button" class="btn btn-primary append-image" @click="addImage">Append Images</button>
+    </div>
 </template>
 
 <script>
-    import draggable from 'vuedraggable'
-    export default {
-        components: {
-            draggable,
-        },
-        props: ['data'],
-        data(){
-            let images = [];
-            let photos = this.data;
-            photos = photos.replace(/(\r\n|\n|\r)/gm, ' ');
-            let data = JSON.parse(photos);
-            if (Object.keys(data)) {
-                if (typeof data.renovation_media != 'undefined') {
-                    const renovation_media = data.renovation_media;
-                    for(let i = 0; i < renovation_media.length; i++) {
-                        const url = renovation_media[i]['url_path'];
-                        images.push([url, renovation_media[i]['description']]);
-                    }
-                }
+import draggable from 'vuedraggable';
+import Vue from 'vue';
+import CKEDITOR from 'ckeditor4-vue';
+
+Vue.use(CKEDITOR);
+
+export default {
+    components: {
+        draggable
+    },
+    props: ['data'],
+    data() {
+        let images = [];
+        let data = this.data;
+        if (typeof data.renovation_media != 'undefined') {
+            const renovation_media = data.renovation_media;
+            for (let i = 0; i < renovation_media.length; i++) {
+                const url = renovation_media[i]['url_path'];
+                images.push([url, renovation_media[i]['description']]);
             }
-            return {
-                images: images,
+        }
+        return {
+            images: images,
+            editorConfig: {
+                toolbar: [
+                    ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat'],
+                    ['NumberedList', 'BulletdList', '-', 'Outdent', 'Indent', '-', 'Blockquote'],
+                    ['Styles', 'Format']
+                ]
             }
+        };
+    },
+    methods: {
+        addImage() {
+            this.images.push(['/images/no-image.png', '']);
         },
-        methods: {
-            addImage(){
-                this.images.push(['/images/no-image.png', '']);
-            },
-            removeImage(idx){
-                this.$delete(this.images, idx);
-            },
-            onFileChange(e) {
-                const file = e.target.files[0];
-                if(file){
-                    this.images[e.target.dataset.indexImage][0] = URL.createObjectURL(file);
-                    this.$forceUpdate();
-                }
+        removeImage(idx) {
+            this.$delete(this.images, idx);
+        },
+        onFileChange(e) {
+            const file = e.target.files[0];
+            if (file) {
+                this.images[e.target.dataset.indexImage][0] = URL.createObjectURL(file);
+                this.$forceUpdate();
             }
         }
     }
+};
 </script>
