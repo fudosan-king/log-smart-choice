8865b4967b2919d20e1a4631adb127c7f2cbfbb3
hungnt <hungnt92@propolife.co.jp>
2021-07-21 15:56:13 +0700

    #9264: Add class announcement

--- app/Jobs/SendEmailAnnouncement.php
+++ app/Jobs/SendEmailAnnouncement.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class SendEmailAnnoucement implements ShouldQueue
+class SendEmailAnnouncement implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
