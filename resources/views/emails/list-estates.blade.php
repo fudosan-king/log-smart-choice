2270b75f784d633165ba33641240b835ad1e3648
hungnt <hungnt92@propolife.co.jp>
2021-09-14 14:30:00 +0700

    HotFix: Remove file not use

--- resources/views/emails/list-estates.blade.php
+++ resources/views/emails/list-estates.blade.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -106,8 +104,6 @@
 </head>
 
 <body paddingwidth="0" paddingheight="0" bgcolor="#F2F2F2" style="padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;" offset="0" toppadding="0" leftpadding="0">
-
-
     <table align="center" bgcolor="#F2F2F2" border="0" cellpadding="0" cellspacing="0" class="tableContent" style="font-family:Georgia, times, sans-serif;" width="100%">
         <tbody>
             <tr>
