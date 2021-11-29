895f4c88a29d9b4bb04fb29fe4432a66739f0c8f
hungnt <hungnt92@propolife.co.jp>
2021-11-29 16:16:03 +0700

    HotFix: Remove log do not need

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -12,7 +12,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        \League\OAuth2\Server\Exception\OAuthServerException::class
     ];
 
     /**
