1be76629ce473f6c48ce0222586bcf269f278cd4
hungnt <hungnt92@propolife.co.jp>
2021-12-10 10:14:22 +0700

    HotFix: Change time send email announcement

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         // $schedule->job(new SendEmailAnnouncement)->everyTwoMinutes();
         // $schedule->job(new SendAnnoucement)->dailyAt('2:00');
         // $schedule->job(new SendAnnoucement)->dailyAt('8:00');
-        $schedule->job(new SendEmailAnnouncement)->dailyAt('9:00');
+        $schedule->job(new SendEmailAnnouncement)->dailyAt('10:00');
     }
 
     /**
