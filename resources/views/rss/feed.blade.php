947f7a4bb0124149f0b60cbbfbca95b8be188eb1
Truong Tam <tam@propolife.co.jp>
2021-12-29 09:27:23 +0700

    Update rss

--- resources/views/rss/feed.blade.php
+++ resources/views/rss/feed.blade.php
@@ -1,5 +1,5 @@
 <?php
-'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
+echo '<?xml version="1.0" encoding="UTF-8"?>'
 ?>
 <rss version="2.0"
   xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -8,24 +8,32 @@
   xmlns:atom="http://www.w3.org/2005/Atom"
   xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
   xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
->
-    <channel>
-        <title><![CDATA[ Order Renove ]]></title>
-        <link><![CDATA[ http://order-renove.jp/feed ]]></link>
-        <description><![CDATA[ Feed ]]></description>
-        <language>ja</language>
-        <pubDate>{{ now() }}</pubDate>
+  >
 
-        @foreach($posts as $post)
-            <item>
-                <title><![CDATA[{{ $post->title }}]]></title>
-                <link>{{ $post->slug }}</link>
-                <description><![CDATA[{!! $post->body !!}]]></description>
-                <category>{{ $post->category }}</category>
-                <author><![CDATA[{{ $post->user  }}]]></author>
-                <guid>{{ $post->id }}</guid>
-                <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
-            </item>
-        @endforeach
-    </channel>
+  <channel>
+    <title>Order Renove</title>
+    <atom:link href="https://order-renove.jp/feed" rel="self" type="application/rss+xml" />
+    <link>https://order-renove.jp/feed</link>
+    <description>Feed</description>
+    <lastBuildDate>{{ now() }}</lastBuildDate>
+    <language>ja</language>
+    <sy:updatePeriod>hourly</sy:updatePeriod>
+    <sy:updateFrequency>1</sy:updateFrequency>
+    <generator>Order Renove</generator>
+
+    @foreach($posts as $post)
+      <item>
+        <title>{{ $post->title }}</title>
+        <link>{{ $post->slug }}</link>
+        <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+        <dc:creator>{{ $post->user  }}</dc:creator>
+        <category>{{ $post->category }}</category>
+        <guid isPermaLink="false">{{ $post->id }}</guid>
+        <description><![CDATA[{!! $post->body !!}]]></description>
+        <content:encoded><![CDATA[{!! $post->body !!}]]></content:encoded>
+        <enclosure url="{!! $post->photo_first !!}" type="image/jpeg" />
+      </item>
+    @endforeach
+
+  </channel>
 </rss>
