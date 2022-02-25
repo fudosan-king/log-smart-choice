38083dfc62fcc5a49d044c0083e1eec2e1c00275
hungnt <hungnt92@propolife.co.jp>
2022-02-25 09:21:47 +0700

    Update ip production

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rsync --delete -e  'ssh -i deployment/fdk-production.pem' -rlpcgz -v --exclude-from=excludes ./ ec2-user@54.238.184.234:/var/www/log-smart-choice/
-ssh ec2-user@54.238.184.234 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && composer install'
-ssh ec2-user@54.238.184.234 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && npm install'
-ssh ec2-user@54.238.184.234 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && php artisan migrate && php artisan db:seed'
-ssh ec2-user@54.238.184.234 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && npm run production'
+rsync --delete -e  'ssh -i deployment/fdk-production.pem' -rlpcgz -v --exclude-from=excludes ./ ec2-user@13.230.141.60:/var/www/log-smart-choice/
+ssh ec2-user@13.230.141.60 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && composer install'
+ssh ec2-user@13.230.141.60 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && npm install'
+ssh ec2-user@13.230.141.60 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && php artisan migrate && php artisan db:seed'
+ssh ec2-user@13.230.141.60 -i deployment/fdk-production.pem -t 'cd /var/www/log-smart-choice && npm run production'
